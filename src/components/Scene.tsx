dc70b08cacc73018a0a97919d24325e4c1682af3
arvee0220 <rowellverdejo@gmail.com>
2025-05-05 20:06:35 +0800

    Add gui helper for modifying sizes and position

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -2,12 +2,50 @@
 
 import { Environment, OrbitControls } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
 import { useRef } from "react";
 import * as THREE from "three";
 
 const Scene = () => {
     const lightRef = useRef<THREE.PointLight>(null);
     const { scene } = useThree();
+
+    const position = useControls("position", {
+        x: {
+            value: 0,
+
+            min: 0,
+            max: 10
+        },
+        y: {
+            value: 0,
+            min: 0,
+            max: 10
+        },
+        z: {
+            value: 0,
+            min: 0,
+            max: 10
+        }
+    });
+
+    const size = useControls("size", {
+        x: {
+            value: 1,
+            min: 0,
+            max: 10
+        },
+        y: {
+            value: 1,
+            min: 0,
+            max: 10
+        },
+        z: {
+            value: 1,
+            min: 0,
+            max: 10
+        }
+    });
 
     useFrame(() => {
         if (lightRef.current) {
@@ -28,8 +66,8 @@ const Scene = () => {
             <axesHelper scale={3} />
 
             {/* Object  */}
-            <mesh>
-                <boxGeometry args={[1, 2, 1]} />
+            <mesh position={[position.x, position.y, position.z]}>
+                <boxGeometry args={[size.x, size.y, size.z]} />
                 <meshPhysicalMaterial roughness={0} side={2} metalness={0.2} color={"gray"} />
             </mesh>
 
